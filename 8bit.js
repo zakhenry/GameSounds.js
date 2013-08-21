9d7226247e51ae72216888f6817661a1208260e7
Cody Lundquist <cody@4mation.com.au>
2013-08-21 10:19:45 +1000

    Implemented proper end of song detection and looping capabilities.

--- 8bit.js
+++ 8bit.js
@@ -26,7 +26,7 @@ EightBit = (function() {
         // Pitch frequencies with corresponding names
         pitches = {'C3': 130.81,'C#3': 138.59,'Db3': 138.59,'D3': 146.83,'D#3': 155.56,'Eb3': 155.56,'E3': 164.81,'F3': 174.61,'F#3': 185.00,'Gb3': 185.00,'G3': 196.00,'G#3': 207.65,'Ab3': 207.65,'A3': 220.00,'A#3': 233.08,'Bb3': 233.08,'B3': 246.94,'C4': 261.63,'C#4': 277.18,'Db4': 277.18,'D4': 293.66,'D#4': 311.13,'Eb4': 311.13,'E4': 329.63,'F4': 349.23,'F#4': 369.99,'Gb4': 369.99,'G4': 392.00,'G#4': 415.30,'Ab4': 415.30,'A4': 440.00,'A#4': 466.16,'Bb4': 466.16,'B4': 493.88,'C5': 523.25,'C#5': 554.37,'Db5': 554.37,'D5': 587.33,'D#5': 622.25,'Eb5': 622.25,'E5': 659.26,'F5': 698.46,'F#5': 739.99,'Gb5': 739.99,'G5': 783.99,'G#5': 830.61,'Ab5': 830.61,'A5': 880.00,'A#5': 932.33,'Bb5': 932.33,'B5': 987.77,'C6': 1046.50,'C#6': 1108.73,'Db6': 1108.73,'D6': 1174.66,'D#6': 1244.51,'Eb6': 1244.51,'E6': 1318.51,'F6': 1396.91,'F#6': 1479.98,'Gb6': 1479.98,'G6': 1567.98,'G#6': 1661.22,'Ab6': 1661.22,'A6': 1760.00,'A#6': 1864.66,'Bb6': 1864.66,'B6': 1975.53,'C7': 2093.00,'C#7': 2217.46,'Db7': 2217.46,'D7': 2349.32,'D#7': 2489.02,'Eb7': 2489.02,'E7': 2637.02,'F7': 2793.83,'F#7': 2959.96,'Gb7': 2959.96,'G7': 3135.96,'G#7': 3322.44,'Ab7': 3322.44,'A7': 3520.00,'A#7': 3729.31,'Bb7': 3729.31,'B7': 3951.07,'C8': 4186.01},
         // Used when parsing the time signature
-        signatureToNote = {
+        signatureToNoteLengthRatio = {
             2: 6,
             4: 3,
             8: 4.50
@@ -57,6 +57,31 @@ EightBit = (function() {
             currentPlayTime,
             totalPlayTime = 0,
             calculatedPlayTime = 0,
+            paused = false,
+            playing = false,
+            loop = false,
+            onFinishedCallback,
+            onFinished = function(cb) {
+                reset();
+                totalPlayTime = 0;
+                if (loop) {
+                    self.play();
+                } else {
+                    cb();
+                }
+            },
+            totalPlayTimeCalculator = function() {
+                setTimeout(function() {
+                    if (! paused && playing) {
+                        if (calculatedPlayTime < totalPlayTime) {
+                            onFinished(onFinishedCallback);
+                        } else {
+                            updateTotalPlayTime();
+                            totalPlayTimeCalculator();
+                        }
+                    }
+                }, 1);
+            },
             /**
              * Instrument Class
              */
@@ -217,16 +242,17 @@ EightBit = (function() {
          * Stop playing all music and reset the Oscillators
          */
         this.stop = function(fadeOut) {
-            totalPlayTime = 0;
             if (typeof fadeOut === 'undefined') {
                 fadeOut = true;
             }
             if (fadeOut) {
                 fade('down', function() {
+                    totalPlayTime = 0;
                     reset();
                     self.unmute();
                 });
             } else {
+                totalPlayTime = 0;
                 reset();
             }
         };
@@ -258,13 +284,14 @@ EightBit = (function() {
          */
         this.end = function() {
             oscillators = [];
+            calculatedPlayTime = 0;
             for (var i = 0; i < allNotesBuffer.length; i++) {
                 var pitch = allNotesBuffer[i].pitch,
                     startTime = allNotesBuffer[i].startTime,
                     stopTime = allNotesBuffer[i].stopTime
                 ;
 
-                calculatedPlayTime += stopTime - startTime;
+                calculatedPlayTime += allNotesBuffer[i].duration;
 
                 // No pitch, then it's a rest and we don't need an oscillator
                 if (! pitch) {
@@ -276,21 +303,21 @@ EightBit = (function() {
                     var o = ac.createOscillator(),
                         volume = ac.createGain();
 
-                    // Connect volume gain to the context;
+                    // Connect volume gain to the Master Volume;
                     volume.connect(masterVolume);
-
                     // Set the volume for this note
                     volume.gain.value = allNotesBuffer[i].volume;
+                    // Connect note to volume
                     o.connect(volume);
-
+                    // Set pitch type
                     o.type = allNotesBuffer[i].pitchType;
+                    // Set frequency
                     o.frequency.value = pitches[p];
-
+                    // Add to the list of oscillators
                     oscillators.push({
                         startTime: startTime,
                         stopTime: stopTime,
-                        o: o,
-                        volume: volume
+                        o: o
                     });
                 });
             }
@@ -303,12 +330,13 @@ EightBit = (function() {
          * @param bottom - What note type has the beat
          */
         this.setTimeSignature = function(top, bottom) {
-            if (typeof signatureToNote[bottom] === 'undefined') {
+            if (typeof signatureToNoteLengthRatio[bottom] === 'undefined') {
                 throw new Error('The bottom time signature is not supported.');
             }
 
+            // Not used at the moment, but will be handy in the future.
             beatsPerBar = top;
-            noteGetsBeat = signatureToNote[bottom];
+            noteGetsBeat = signatureToNoteLengthRatio[bottom];
         };
 
         /**
@@ -325,16 +353,21 @@ EightBit = (function() {
          * It will use the total time played so far as an offset so you pause/play the music
          */
         this.play = function() {
+            playing = true;
+            paused = false;
+            // Starts calculator which keeps track of total play time
             currentPlayTime = ac.currentTime;
+            totalPlayTimeCalculator();
 
             var timeOffset = ac.currentTime - totalPlayTime;
             oscillators.forEach(function(osc) {
                 var o = osc.o,
-                    startTime = osc.startTime,
-                    stopTime = osc.stopTime
+                    startTime = osc.startTime + timeOffset,
+                    stopTime = osc.stopTime + timeOffset
                 ;
-                o.start(startTime + timeOffset);
-                o.stop(stopTime + timeOffset);
+
+                o.start(startTime);
+                o.stop(stopTime);
             });
 
             if (totalPlayTime > 0) {
@@ -343,13 +376,36 @@ EightBit = (function() {
         };
 
         /**
+         * Set a callback to fire when the song is finished
+         *
+         * @param cb
+         */
+        this.onFinished = function(cb) {
+            if (typeof cb !== 'function') {
+                throw new Error('onFinished callback must be a function.');
+            }
+
+            onFinishedCallback = cb;
+        };
+
+        /**
          * Pauses the music, resets the oscillators, gets the total time played so far
          */
         this.pause = function() {
-            totalPlayTime += ac.currentTime - currentPlayTime;
+            paused = true;
+            updateTotalPlayTime();
             fade('down', function() {
                 reset();
             });
+        };
+
+        /**
+         * Set true if you want the song to loop
+         *
+         * @param l
+         */
+        this.loop = function(l) {
+            loop = l;
         };
 
         // Default to 120 tempo
@@ -357,6 +413,14 @@ EightBit = (function() {
 
         // Default to 4/4 time signature
         this.setTimeSignature(4, 4);
+
+        /**
+         * Call to update the total play time so far
+         */
+        function updateTotalPlayTime() {
+            totalPlayTime += ac.currentTime - currentPlayTime;
+            currentPlayTime = ac.currentTime;
+        }
 
         /**
          * Helper function to figure out how long a note is
@@ -372,6 +436,7 @@ EightBit = (function() {
          * Helper function to stop all oscillators and recreate them
          */
         function reset() {
+            playing = false;
             oscillators.forEach(function(osc) {
                 osc.o.stop(0);
             });
@@ -393,6 +458,7 @@ EightBit = (function() {
                     setTimeout(function() {
                         if (i > 0) {
                             i = i - 2;
+                            i = i < 0 ? 0 : i;
                             var gain = 'up' === direction ? masterVolumeLevel * 100 - i : i;
                             masterVolume.gain.value = gain / 100;
                             timeout();
